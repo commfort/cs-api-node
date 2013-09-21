6610dbec9928a5fbe37549ae9d93d3f9ce2ac1cc
Chris McKnight <cmckni3@gmail.com>
2013-09-21 15:43:30 -0500

    Fix output example

--- actions/members.js
+++ actions/members.js
@@ -48,7 +48,7 @@ exports.membersChallenges = {
     optional: [],
   },
   authenticated: false,
-  outputExample: { { "0" : { "id": "2885", "name": "Port the CloudSpokes API to Node.js", "challenge_type" : "Code", "top_prize": "100" } },
+  outputExample: { "0" : { "id": "2885", "name": "Port the CloudSpokes API to Node.js", "challenge_type" : "Code", "top_prize": "100" } },
   version: 2.0,
   run: function(api, connection, next){
     api.members.challenges(connection.params.membername, function(data){
